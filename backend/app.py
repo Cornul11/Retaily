d062a2cb80d57fc85c8958e2fa9480b1b7066aee
GWNLekkah <a.j.dekker.5@student.rug.nl>
2020-03-31 21:44:36 +0200

    Prepared for returning json strings

--- backend/app.py
+++ backend/app.py
@@ -1,34 +1,60 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 # app configurations for database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@localhost/dbname?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tm69H1sxd0:xnSlasPerh@remotemysql.com:3306/tm69H1sxd0?charset=utf8mb4'
 
 db = SQLAlchemy(app)
 
 
 # product table in database
 class Product(db.Model):
-    plu = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    plu = db.Column(db.Integer)
     name = db.Column(db.String(50))
-    buying_price = db.Column(db.Integer)
-    selling_price = db.Column(db.Integer)
-    discount = db.Column(db.Integer)
-    transaction_id = db.Column(db.Integer, db.ForeignKey('transaction.id'))
+    buying_price = db.Column(db.Integer, nullable=True)
+    selling_price = db.Column(db.Integer, nullable=True)
+    discount = db.Column(db.Integer, nullable=True)
+    transaction_id = db.Column(db.Integer, db.ForeignKey('transaction.id'), nullable=True)
+
+    @property
+    def serialized(self):
+        return {
+            'plu': self.plu,
+            'name': self.name,
+            'buying_price': self.buying_price,
+            'selling_price': self.selling_price,
+            'discount': self.discount,
+            'transaction_id': self.transaction_id,
+        }
+
+
 
 # transaction table in database
 class Transaction(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_time = db.Column(db.DateTime)
-    receipt_number = db.Column(db.Integer)
-    total_amount = db.Column(db.Integer)
-    card_serial = db.Column(db.Integer)
-    change = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date_time = db.Column(db.DateTime, nullable=True)
+    receipt_number = db.Column(db.Integer, nullable=True)
+    total_amount = db.Column(db.Integer, nullable=True)
+    card_serial = db.Column(db.Integer, nullable=True)
+    change = db.Column(db.Integer, nullable=True)
     products = db.relationship('Product', backref='contains')
+
+    @property
+    def serialized(self):
+        return {
+            'id': self.id,
+            'date_time': self.date_time,
+            'receipt_number': self.receipt_number,
+            'total_amount': self.total_amount,
+            'card_serial': self.card_serial,
+            'change': self.change,
+        }
 
 
 @app.route('/')
