b550916c83f60b1db3804906c6756fd3f332dcb9
Dan <cornul11@gmail.com>
2020-04-01 02:15:50 +0200

    added code for inserting to the db from within the parser

--- journal_parser/main.py
+++ journal_parser/main.py
@@ -1,6 +1,7 @@
 import pprint
 import sys
 import time
+import mysql.connector
 
 
 def parse_file(filepath):
@@ -234,6 +235,32 @@ def parse_card_payment(product):
         for key, value in CARD_PAYMENT_ITEMS.items():
             if value in line:
                 cp[key] = line.split(": ")[-1].strip()
+    # from mysql.connector import Error
+    # from mysql.connector import errorcode
+    # try:
+    #     connection = mysql.connector.connect(host='remotemysql.com',
+    #                                          database='tm69H1sxd0',
+    #                                          user='tm69H1sxd0',
+    #                                          password='xnSlasPerh')
+    #     print('card_usage_date = ', cp['cp_date'])
+    #     print('card_transaction_id = ', cp['cp_transaction'])
+    #     print('card_total_amount = ', cp['cp_drawer_amount'])
+    #     print('card_serial =', cp['cp_card_serial_number'])
+    #     from datetime import datetime
+    #     datetim = datetime.strptime(cp['cp_date'], '%d/%m/%Y %H:%M')
+    #     mysql_insert_query = """INSERT INTO transaction (date_time, receipt_number, total_amount, card_serial) VALUES ('%s', '%d', '%f', '%d')""" % (
+    #         datetim, int(cp['cp_transaction']), float(cp['cp_drawer_amount'].replace(',', '.')), int(cp['cp_card_serial_number']))
+    #     cursor = connection.cursor()
+    #     cursor.execute(mysql_insert_query)
+    #     connection.commit()
+    #     print(cursor.rowcount, "Record inserted succesfully")
+    #     cursor.close()
+    # except mysql.connector.Error as error:
+    #     print("Failed to insert record into table {}".format(error))
+    # finally:
+    #     if connection.is_connected():
+    #         connection.close()
+    #         print('Mysql connection closed')
     return cp
 
 
