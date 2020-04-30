ec4711a48377696c741d0c53d9a2ef70b7e53976
Dan <cornul11@gmail.com>
2020-04-30 03:37:15 +0200

    replaced background, minor code refactoring, style improvements

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import NavBar from "./../components/design/NavBar";
 import "./../components/design/fancyButton.css";
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 class HomePage extends Component{
     render(){
@@ -9,15 +8,15 @@ class HomePage extends Component{
             <div>
                 <NavBar />
                 <div className="container">
-                <div className="card border-0 shadow my-5">
-                    <div className="card-body p-5">
-                    {/*----- put page content under this line -----*/}
-                    <a href="/simple/productinfo"> <button className="btn btn-primary btn-block mb-2" > Simple Product Info</button></a>
-                    <a href="/extended/productinfo"> <button className="btn btn-primary btn-block mb-2" > Extended Product Info</button></a>
-                    <a href="/inventorybarchart"> <button className="btn btn-primary btn-block mb-2" >Overview of sales</button></a>
-                    {/*----- page content end -----*/}
+                    <div className="card border-0 shadow my-5">
+                        <div className="card-body p-5">
+                            {/*----- put page content under this line -----*/}
+                            <a className="btn btn-primary btn-block mb-2" href="/simple/productinfo">Simple Product Info</a>
+                            <a className="btn btn-primary btn-block mb-2" href="/extended/productinfo">Extended Product Info</a>
+                            <a  className="btn btn-primary btn-block mb-2" href="/inventorybarchart">Overview of sales</a>
+                            {/*----- page content end -----*/}
+                        </div>
                     </div>
-                </div>
                 </div>
             </div>
         );
