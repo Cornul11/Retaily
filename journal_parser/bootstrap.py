512bd4bd83341d9d5aed49049027ca4f1455fc76
Cornul11 <cornul11@gmail.com>
2020-06-04 10:40:21 +0200

    merged folder 0001 with 0002 for improved sorting

--- journal_parser/bootstrap.py
+++ journal_parser/bootstrap.py
@@ -21,11 +21,12 @@ def process(src_path):
 if __name__ == '__main__':
     src_path = sys.argv[1] if len(sys.argv) > 1 else '.'
     print('Journal parser for uploading started at: ', time.ctime())
+    start_time = time.time()
     print('Uploading everything in ', src_path)
     files_in_0001 = [src_path + '/0001/' + file for file in os.listdir(src_path + '/0001')]
-    files_in_0002 = [src_path + '/0002/' + file for file in os.listdir(src_path + '/0002')]
+    #files_in_0002 = [src_path + '/0002/' + file for file in os.listdir(src_path + '/0002')]
     files_in_0001.sort()
-    files_in_0002.sort()
+    #files_in_0002.sort()
 
     print('0001:')
     with tqdm(files_in_0001) as pbar:
@@ -34,9 +35,12 @@ if __name__ == '__main__':
             time.sleep(0.1)
             process(filename)
 
-    print('0002:')
-    with tqdm(files_in_0002) as pbar:
-        for filename in pbar:
-            pbar.set_postfix_str(s=filename[-27:], refresh=True)
-            time.sleep(0.1)
-            process(filename)
+    #print('0002:')
+    #with tqdm(files_in_0002) as pbar:
+    #    for filename in pbar:
+    #        pbar.set_postfix_str(s=filename[-27:], refresh=True)
+    #        time.sleep(0.1)
+    #        process(filename)
+    
+    print('--- %s seconds ---' % (time.time() - start_time))
+	
